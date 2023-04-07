1452bf455a975ee8ae2b2d4240171e7c15eecac3
varya <varyadaser@gmail.com>
2023-04-07 12:30:37 +0400

    hadnlers ?

---
+++ src/handlers/signIn.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+
+const signIn = (req: Request, res: Response) => {
+}
+
+export { signIn }
---
+++ src/handlers/signUp.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+
+const signUp = (req: Request, res: Response) => {
+}
+
+export { signUp }
