f9e4ba533cab081c4f6546db7c8da820af432cad
varya <varyadaser@gmail.com>
2023-04-07 12:31:23 +0400

    signin signup routed

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,9 +1,16 @@
 import * as express from "express";
 import { Request, Response } from "express";
+import { signUp } from "../handlers/signUp"
+import { signIn } from "../handlers/signIn"
 const router = express.Router();
 
+// ignore this for now
 router.get("/", (req: Request, res: Response) => {
     res.json({ message: "tptp" })
 });
+
+router.post("/sign-up", signUp);
+
+router.post("/sign-in", signIn)
 
 export { router };
