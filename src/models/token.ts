5e4736699506a7633e9bbf6e7a1e303cdeb338e4
varya <varyadaser@gmail.com>
2023-04-08 18:52:07 +0400

    jwt tokens

---
+++ src/models/token.ts
@@ -0,0 +1,27 @@
+import { Schema, model, Types, Model } from "mongoose"
+
+interface Token {
+    userId: Types.ObjectId;
+    token: string;
+    createdAt: Date;
+};
+
+const tokenSchema = new Schema<Token, Model<Token>>({
+    userId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: "user",
+    },
+    token: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 3600,
+    },
+});
+
+const TokenModel = model('User', tokenSchema);
+export { TokenModel }
