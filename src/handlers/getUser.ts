f7ec9c4a7de88961ad34ac7aaf52d1e888c8c427
varya <varyadaser@gmail.com>
2023-04-18 16:17:21 +0400

    changed stuff, added more error handlig

--- src/handlers/getUser.ts
+++ src/handlers/getUser.ts
@@ -1,20 +1,16 @@
 import { Response } from "express";
-import mongoose from "mongoose";
-import morgan from "morgan";
 import { User, UserModel } from "../models/user";
 import { AuthRequest } from "./userManagment/auth";
 
 const getProfile = async (req: AuthRequest, res: Response) => {
-  const userId = req.body.user.userID
-
   try {
-    const user = await UserModel.findById(userId)
-    if (!user){
-      return res.status(401).json({ "message": "unauthorized" });  
-    }
-    return res.status(200).json({ user });
+    const userId = req.body.user.userID
+    const user = await UserModel.findById(userId);
+
+    res.status(200).json({ user });
   } catch (err) {
-    console.log(err)
+    console.error(err);
+    res.status(500).json({ "message": "Server error" });
   }
 };
 
