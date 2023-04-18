b2edcaade384ad289536a08514f1dceb4d912dc9
varya <varyadaser@gmail.com>
2023-04-18 14:37:44 +0400

    getProfile implemented

--- src/handlers/getUser.ts
+++ src/handlers/getUser.ts
@@ -1,21 +1,21 @@
-import { Request, Response, NextFunction } from "express";
-import { User } from "../models/user";
+import { Response } from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import { User, UserModel } from "../models/user";
 import { AuthRequest } from "./userManagment/auth";
-// dotenv.config();
 
-// create an instance of the express application
+const getProfile = async (req: AuthRequest, res: Response) => {
+  const userId = req.body.user.userID
 
-// set up a secret for signing and verifying JWT tokens
-// Storing the token in LocalStorage is fine. If you need to fetch the user details,
-// create an endpoint in your API such as getUser. You can then use
-// jwt.decode(accessToken, JWT SECRET HERE) and return the decoded value
-// (which will be your user) assuming the accessToken is valid.
-
-// ok, pochti ponimayu??
-
-const getProfile = (req: AuthRequest, res: Response) => {
-  console.log(req.body.user.userID);
-  res.json({ user: "asd" });
+  try {
+    const user = await UserModel.findById(userId)
+    if (!user){
+      return res.status(401).json({ "message": "unauthorized" });  
+    }
+    return res.status(200).json({ user });
+  } catch (err) {
+    console.log(err)
+  }
 };
 
 export { getProfile };
